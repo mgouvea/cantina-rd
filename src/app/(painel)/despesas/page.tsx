3f3cd039b4a8f53bd97d50c775a6d43edba3e169
Mateus Gouvea <falamateus.gouvea@gmail.com>
2025-08-05 15:21:21 -0300

    feat: create basic expenses page with title component

---
+++ src/app/(painel)/despesas/page.tsx
@@ -0,0 +1,12 @@
+import Text from "@/app/components/ui/text/Text";
+import React from "react";
+
+const Despesas = () => {
+  return (
+    <div>
+      <Text variant="h5">Despesas</Text>
+    </div>
+  );
+};
+
+export default Despesas;
